3a6c51ba604a0079b3feff89f76e27d14dcdf279
Abideen Olafimihan <olafimihana@gmail.com>
2022-05-19 14:41:51 +0100

    finding the position of data in an array

---
+++ findThePosition.js
@@ -0,0 +1,16 @@
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says: "found the needle at position " plus the index it found the needle, so:
+
+// Function takes an array of containing the word 'needle'
+// return "found the needle at position + index"
+
+function findNeedle(haystack) {
+    // your code here
+    if (haystack.includes('needle')) // check if its true
+    return `found the needle at position ${haystack.indexOf('needle')}`
+  }
+  
+  
+  //Example
+  findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
