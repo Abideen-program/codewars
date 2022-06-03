e6fde12dbae405a770364d059fa23622ec24f0b6
Abideen Olafimihan <olafimihana@gmail.com>
2022-06-03 10:26:14 +0100

    turning each letter to uppercase

---
+++ makeUpperCase.js
@@ -0,0 +1,8 @@
+//Change the letters of a string to uppercase
+//split the string
+//run a map function 
+//add toUppercase to each item
+//Join each item back
+const makeUpperCase = (str) => str.split('').map( item => item.toUpperCase()).join('')
+
+console.log(makeUpperCase('abideen'))
