8d3deda324592c50cca44caddc6f3b7710c40adc
Abideen Olafimihan <olafimihana@gmail.com>
2022-06-15 14:49:25 +0100

    get the vowel count

---
+++ vowelCount.js
@@ -0,0 +1,17 @@
+function getCount(str) {
+    let vowelsCount = 0;
+
+          str.toLowerCase() //convert string to lowercase
+          .split('')
+          .forEach( vowel => { // loop through each array and check if they contains 'a,e,i,o,u'
+            if (vowel === 'a' | vowel === 'e' | vowel === 'i' | vowel === 'o' | vowel === 'u') {
+                vowelsCount++; // increase the vowel count
+          }
+      })
+    return vowelsCount;
+  }
+  getCount('abracadabra')
+  getCount('Olafimihan')
+  getCount('ABIDEEN OLAFIMIMIHAN')
+  
+  
