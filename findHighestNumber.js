25f5735c9b0be51841503265bc0cc5730a938ebb
Abideen Olafimihan <olafimihana@gmail.com>
2022-07-03 12:03:29 +0100

    finding the heighest number in an array

---
+++ findHighestNumber.js
@@ -0,0 +1,28 @@
+
+const numbers = [1, 2, 3, 4, 6, 9, 50, 7, 10]
+
+const max =  (numbers) => {
+    // declear a result to store an initial result
+    let result = 0
+    // lop throught the array of numbers
+    for (const number of numbers) {
+        /* 
+            compare each value of the array to the value of the result
+            if the value of the array is greater than that of the result:
+                set the value of the value of the array as that of the result
+
+            else if the value of the array is not greater, then retain the value of the result 
+
+        */
+        if ( number > result) {
+            result = number
+        } else {
+            result = result
+        }
+    }
+        
+    // then return the result
+    return {result}
+}
+
+max(numbers)
