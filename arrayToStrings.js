0a073d1599505756e546afeb8cb60c9300691ac0
Abideen Olafimihan <olafimihana@gmail.com>
2022-09-19 22:57:34 +0100

    changing an array to a string

---
+++ arrayToStrings.js
@@ -0,0 +1,34 @@
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a
+string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+Your tasks:
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+string like the above to the console. Try it with both test datasets.
+2. Use the problem-solving framework: Understand the problem and break it up
+into sub-problems!
+Test data:
+§ Data 1: [17, 21, 23]
+§ Data 2: [12, 5, -5, 0, 4]
+GOOD LUCK 😀
+*/
+
+//  declear an empty string
+//  loop through the array
+//  add each item in the array to the empty string using (+=)
+//  increase the number of days by using (i + 1) and add to the string
+//  return string
+
+
+
+const printForecast = (array) => {
+    let output = '';
+    for (let i = 0; i < array.length; i++) {
+      output += `${array[i]}ºC in ${i + 1} days ... `
+    }
+  
+    return ('... ' + output);
+  }
+  
+  console.log(printForecast([17, 21, 23]))
+  console.log(printForecast([12, 5, -5, 0, 4]))
