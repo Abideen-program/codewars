17568b4d8ae4a42f1a0dbdb8047896c7ccdb6fcb
Abideen Olafimihan <olafimihana@gmail.com>
2022-10-09 19:12:16 +0100

    solving the third challenge

--- challenge.js
+++ challenge.js
@@ -110,6 +110,7 @@ Lewandowski: 2
 }
 */
 
+/* 
 for (const [goal, player] of game.scored.entries()) {
   console.log(`Goal ${goal + 1}: ${player}`);
 }
@@ -126,10 +127,53 @@ for (const [team, odd] of Object.entries(game.odds)) {
   console.log(`Odd of ${outPut}: ${odd}`);
 }
 
-const scorers = {}
 
-for (const player of game.scored){
-    scorers[player] = scorers[player] ? scorers[player] + 1 : 1
+const scorers = {};
+
+for (const player of game.scored) {
+  scorers[player] = scorers[player] ? scorers[player] + 1 : 1;
 }
+console.log(scorers);
+*/
 
-console.log(scorers)
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+/*
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17:⚽ GOAL
+*/
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+const time = [...gameEvents.keys()].pop();
+
+console.log(`An event happened, on
+average, every ${time / gameEvents.size} minutes`);
+
+const newGameEvent = [...gameEvents.entries()];
+
+for (const [time, event] of newGameEvent) {
+  const output = time <= 45 ? "[FIRST HALF]" : "[SECOND HALF]";
+  console.log(`${output} ${time}: ${event}`);
+}
