fb7f8932955c789043affcb4e0b11a710e1ed013
Abideen Olafimihan <olafimihana@gmail.com>
2022-10-10 18:11:48 +0100

    solving more string challenge

--- stringChallenge.js
+++ stringChallenge.js
@@ -25,7 +25,7 @@ Hints:
 name conversion working 😉
 § This challenge is difficult on purpose, so start watching the solution in case
 you're stuck. Then pause and continue!
-*/
+
 
 const formatText = (message) => {
   const formatedMessage = message.trim().toLowerCase().split("_");
@@ -41,3 +41,18 @@ formatText("first_name");
 formatText("Some_Variable");
 formatText("calculate_AGE");
 formatText("delayed_departure");
+*/
+
+const flights =
+  "_Delayed_Departure;fao9574754;txl125645;11:25+_Arrival;bru02589;fao58945;11:45+_Delayed_Arrival;hel954855;fao954265;12:05+_Departure;fao78595;lis5896314;12:30";
+
+  const changeCase = (code) => code.slice(0, 3).toUpperCase()
+
+for (const flight of flights.split("+")) {
+
+    const [type, from, to, time] = flight.split(';');
+    const output = (`${type.startsWith('_Delayed') ? `🔴` : '' }${type.replaceAll('_', ' ')} from ${changeCase(from)} to ${changeCase(to)} (${time.replace(':', 'h')})`).padStart(45)
+
+    console.log(output);
+}
+
