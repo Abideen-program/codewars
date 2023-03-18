82cebfbf5beb65e4a1387cd65ff1e67a0addab9d
Abideen Olafimihan <olafimihana@gmail.com>
2023-03-18 15:46:09 +0530

    alphabetPosition

---
+++ alphabetPosition.js
@@ -0,0 +1,17 @@
+const alphabetPosition = (text) => {
+  //write out the alphabets
+  let alphabets = "abcdefghijklmnopqrstuvwxyz";
+  //change the string of alphabets into arrays
+  let alphabetArray = alphabets.split("");
+  //change the input text into an array
+  let textArray = text.toLowerCase().split("");
+  return textArray
+    .filter((letter) => {
+      return alphabetArray.includes(letter); //check for the members that are empty spaces don't include them
+    })
+    .map((letter) => { //check the index of the returned array and add 1 to each
+      return alphabetArray.indexOf(letter) + 1; 
+    });
+};
+
+console.log(alphabetPosition("abcdh"));
