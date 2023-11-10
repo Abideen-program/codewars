2a1118c0623619729dc660cce398b88e318887c5
Abideen Olafimihan <olafimihana@gmail.com>
2023-11-10 18:17:31 +0100

    created goosefilter

---
+++ findNextSquare.js
@@ -0,0 +1,8 @@
+function findNextSquare(sq) {
+    // Return the next square if sq is a perfect square, -1 otherwise
+    const number = Math.sqrt(sq) // find the square root of the input value
+    if(Number.isInteger(number)) { //check if the number does not have decimal
+      return (number + 1)*(number + 1) //then return the square of the number
+    }
+    return -1;
+  }
