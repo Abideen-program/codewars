59e4b118e4d193f4b338ffab5a96cde4bfa2a4c6
Abideen Olafimihan <olafimihana@gmail.com>
2023-11-14 20:46:10 +0100

    changing boolean to strings

--- booleanToString.ts
+++ booleanToString.ts
@@ -1,12 +1,12 @@
 // Implement a function which convert the given boolean value into its string representation.
 
-function booleanToString(b){
+function booleanToString(b: boolean){
     return (b === true) ? "true" : "false";
 }
 
 // OR
 
-function booleanToString(b){
+function booleanToStrings(b: boolean){
     return b.toString();
 }
 
