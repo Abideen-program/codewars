cdfcd54310edb258fcf78ac0730807afe5810a65
Abideen Olafimihan <olafimihana@gmail.com>
2024-01-05 22:15:05 +0100

    changed to typescript

--- filteringArray.ts
+++ filteringArray.ts
@@ -1,6 +1,6 @@
 //Make a program that filters a list of strings and returns a list with only your friends name in it. If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
 
-function friend(friends){
+function friend(friends: string[]){
     return friends.filter( name => name.length === 4) //check for a string in the array with length of 4
 }
 
