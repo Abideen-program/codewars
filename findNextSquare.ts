b95936ccef08c9e7a2eec544d831b91a98590f4d
Abideen Olafimihan <olafimihana@gmail.com>
2024-01-06 18:13:18 +0100

    changed to typescript

--- findNextSquare.ts
+++ findNextSquare.ts
@@ -1,4 +1,4 @@
-function findNextSquare(sq) {
+function findNextSquare(sq:number) {
     // Return the next square if sq is a perfect square, -1 otherwise
     const number = Math.sqrt(sq) // find the square root of the input value
     if(Number.isInteger(number)) { //check if the number does not have decimal
