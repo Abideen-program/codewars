79b9fe779181eb1ee70c2e25513ef6bd05f3598e
Abideen Olafimihan <olafimihana@gmail.com>
2024-01-07 15:02:33 +0100

    changed to typescript

---
+++ isPalindrome.ts
@@ -0,0 +1,14 @@
+const isPalendrom = (string: string) => {
+  // covert the input string to a lowercase
+  const stringLow = string.toLowerCase();
+  const array = stringLow
+    .split("") // convert stringLow to an array
+    .reverse() // reverse the result of the conversion
+    .join(""); // join back together to form a string
+  //compare the array with the stringLow
+  if (stringLow === array) {
+    return true;
+  }
+  return false;
+};
+isPalendrom("racecar");
