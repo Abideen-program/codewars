521c116cf397af7c0a07af86cf904b912360a426
Abideen Olafimihan <olafimihana@gmail.com>
2024-02-18 18:16:55 +0100

    clear

--- stringChallenge.ts
+++ stringChallenge.ts
@@ -46,13 +46,17 @@ formatText("delayed_departure");
 const flights =
   "_Delayed_Departure;fao9574754;txl125645;11:25+_Arrival;bru02589;fao58945;11:45+_Delayed_Arrival;hel954855;fao954265;12:05+_Departure;fao78595;lis5896314;12:30";
 
-  const changeCase = (code) => code.slice(0, 3).toUpperCase()
+const changeCase = (code: string) => code.slice(0, 3).toUpperCase();
 
 for (const flight of flights.split("+")) {
+  const [type, from, to, time] = flight.split(";");
+  const output = `${type.startsWith("_Delayed") ? `🔴` : ""}${type.replaceAll(
+    "_",
+    " "
+  )} from ${changeCase(from)} to ${changeCase(to)} (${time.replace(
+    ":",
+    "h"
+  )})`.padStart(45);
 
-    const [type, from, to, time] = flight.split(';');
-    const output = (`${type.startsWith('_Delayed') ? `🔴` : '' }${type.replaceAll('_', ' ')} from ${changeCase(from)} to ${changeCase(to)} (${time.replace(':', 'h')})`).padStart(45)
-
-    console.log(output);
+  console.log(output);
 }
-
