ad0504c75ccad07e7992d60fcdb983755b2cb6c5
Abideen Olafimihan <olafimihana@gmail.com>
2024-03-09 21:03:27 +0100

    vowel count changed to ts

--- vowelCount.ts
+++ vowelCount.ts
@@ -1,10 +1,10 @@
-function getCount(str) {
+function getCount(str: any) {
     let vowelsCount = 0;
 
           str.toLowerCase() //convert string to lowercase
           .split('')
           .forEach( vowel => { // loop through each array and check if they contains 'a,e,i,o,u'
-            if (vowel === 'a' | vowel === 'e' | vowel === 'i' | vowel === 'o' | vowel === 'u') {
+            if (vowel === 'a' || vowel === 'e' || vowel === 'i' || vowel === 'o' || vowel === 'u') {
                 vowelsCount++; // increase the vowel count
           }
       })
